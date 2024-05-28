e71605825e6c3277cd421839329b31e7b7bb7db0
iskipu <rvamsi599+github@gmail.com>
2024-05-28 14:03:35 +0530

    understanding outlet, layout and isActive in NavLink

--- 04ReactRouterPractice/src/App.jsx
+++ 04ReactRouterPractice/src/App.jsx
@@ -1,14 +1,52 @@
 import { useEffect, useState } from "react";
 import About from "./About";
 import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
+import {
+  NavLink,
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useOutlet,
+  useOutletContext,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
+      <nav className="flex space-x-4">
+        <ul className="flex space-x-4">
+          <li>
+            <NavLink to="nested" className="text-blue-500 hover:text-blue-800">
+              Nested
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="nested/a"
+              className={({ isActive }) =>
+                ` ${
+                  isActive ? "text-red-500" : "text-blue-400"
+                } hover:text-blue-800`
+              }
+            >
+              NestedA
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="nested/b"
+              className="text-blue-500 hover:text-blue-800"
+            >
+              NestedB
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/user/:user" element={<Home />} />
-        <Route path="/nested">
+        <Route path="/nested" element={<Layout />}>
           <Route index element={<Nested />} />
           <Route path="A" element={<NestedA />} />
           <Route path="B" element={<NestedB />} />
@@ -21,8 +59,17 @@ function App() {
 
 export default App;
 
+function Layout() {
+  return (
+    <>
+      <h1>My Layoutt</h1>
+      <Outlet context={"data from Outlet"} />
+    </>
+  );
+}
 function Nested() {
-  return <>Nested</>;
+  const outletData = useOutletContext();
+  return <>Nested {outletData}</>;
 }
 function NestedA() {
   return <>Nested A</>;
