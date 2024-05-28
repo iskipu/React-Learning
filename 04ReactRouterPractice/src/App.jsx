de5e4f0708edf2f253f6d472f9000149488b1c1d
iskipu <rvamsi599+github@gmail.com>
2024-05-28 13:23:45 +0530

    understanding nested routes, index route and wildcard route (404)

--- 04ReactRouterPractice/src/App.jsx
+++ 04ReactRouterPractice/src/App.jsx
@@ -8,9 +8,25 @@ function App() {
     <>
       <Routes>
         <Route path="/user/:user" element={<Home />} />
+        <Route path="/nested">
+          <Route index element={<Nested />} />
+          <Route path="A" element={<NestedA />} />
+          <Route path="B" element={<NestedB />} />
+        </Route>
+        <Route path="*" element={<h1>NOT FOUNDD 404</h1>} />
       </Routes>
     </>
   );
 }
 
 export default App;
+
+function Nested() {
+  return <>Nested</>;
+}
+function NestedA() {
+  return <>Nested A</>;
+}
+function NestedB() {
+  return <>Nested B</>;
+}
